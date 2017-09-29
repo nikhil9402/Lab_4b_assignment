34c3ae9045b2af5dda5048c82624cf4d6496d294
Nikhil Kumar <31308077+nikhil9402@users.noreply.github.com>
2017-09-29 19:48:12 +0530

    lab_4b_added*

---
+++ lab_4b_question_13.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+#include <iomanip>
+using std::setw;
+
+void array_fn(int arr[], int start, int len)
+{
+    if  (start >= len)
+    {
+        return ;
+    }
+    else
+    {
+        cout << setw( 4 ) << start << setw(13 ) << arr[start] << endl;
+        array_fn(arr, start+1, len);
+    }
+}
+
+int main () {
+    int arr[10],N,i;
+  cout << "Enter length of the array : ";
+  cin >> N;
+  cout << "Enter elements of the array : ";
+  for (i=0;i<N;i++)
+  {
+      cin >> arr[i];
+  }
+
+
+  cout << "Element" << setw( 13 ) << "Value" << endl;
+  array_fn(arr,0,N);
+
+
+
+return 0;
+}
---
+++ lab_4b_question_14.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int sum_fn(int arr[],int start,int len)
+{
+
+    if (start>=len)
+    {
+      return 0;
+    }
+
+    return sum_fn(arr,start+1,len) + arr[start];
+}
+
+
+int main () {
+  int arr[10],N,i;
+  cout << "Enter length of the array : \n";
+  cin >> N;
+  cout << "Enter elements of the array : \n";
+  for (i=0;i<N;i++)
+  {
+      cin >> arr[i];
+  }
+
+  cout << "Sum of the elements of the array is " <<  sum_fn(arr,0,N) << endl;
+ return 0;
+}
+
